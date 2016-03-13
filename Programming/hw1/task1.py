97465c67d334533b93b53709b3a11c4bf95e5f59
Iva Dobreva <iva95dobreva@gmail.com>
2016-03-14 01:34:58 +0900

    Boring first homework

--- Programming/hw1/task1.py
+++ Programming/hw1/task1.py
@@ -1,3 +1,5 @@
+#TASK1
+
 ''' Modify your first program to output the following lines:
 Hello, world!
 My name is xxxx!
@@ -5,7 +7,5 @@ My name is xxxx!
 Hints: your firstname and lastname should be printed instead of 'xxxx'.
 Between each two words, output one blank ' ' character. '''
 
-firstname = input('Enter your first name: ')
-lastname = input('Enter your last name: ')
-
-print('Hello, world!\nMy name is', firstname, ' ', lastname)
+#Print function that outputs the following line
+print('Hello, world!\nMy name is Iva Dobreva!')
