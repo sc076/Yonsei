ec861d1dc6d423c135aa290e86b1ea0d86844bc5
Iva Dobreva <iva95dobreva@gmail.com>
2016-05-23 19:08:43 +0900

    Finished program

--- Programming/lab10/CellularAutomatonTemplate.py
+++ Programming/lab10/CellularAutomatonTemplate.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 import time
 
 SIZE = 20 # size of the 2D cellular automaton
@@ -17,23 +18,43 @@ class Cell:
     def getState(self):
         return self.state
 
-def generateZero(world):
-    world[0][0].changeState(ALIVE)
-    world[1][0].changeState(ALIVE)
-    world[2][0].changeState(ALIVE)
-
-    world[10][11].changeState(ALIVE)
-    world[10][12].changeState(ALIVE)
-    world[10][13].changeState(ALIVE)
-
-    world[11][11].changeState(ALIVE)
-
-    world[12][11].changeState(ALIVE)
-    world[12][12].changeState(ALIVE)
-    world[12][13].changeState(ALIVE)
+def neighboursState(world, row, column):
+    counter = 0
+    try:
+        if world[row-1][column].getState() == ALIVE: counter += 1
+    except:
+        pass
+    try:
+        if world[row][column-1].getState() == ALIVE: counter += 1
+    except:
+        pass
+    try:
+        if world[row][column+1].getState() == ALIVE: counter += 1
+    except:
+        pass
+    try:
+        if world[row+1][column].getState() == ALIVE: counter += 1
+    except:
+        pass
+    try:
+        if world[row-1][column+1].getState() == ALIVE: counter += 1
+    except:
+        pass
+    try:
+        if world[row-1][column-1].getState() == ALIVE: counter += 1
+    except:
+        pass
+    try:
+        if world[row+1][column-1].getState() == ALIVE: counter += 1
+    except:
+        pass
+    try:
+        if world[row+1][column+1].getState() == ALIVE: counter += 1
+    except:
+        pass
+    return counter
 
 def printSep(border):
-    '''Print a separator'''
     for ctr in range(0, border+2):
         print('-', end='')
     print('')
@@ -48,20 +69,47 @@ def printWorld(world):
         print('|', 'row', row)
 
     printSep(len(world))
-    #print('printWorld not yet implemented')
+
+def generateZero(world):
+    world[0][1].changeState(ALIVE)
+    world[1][1].changeState(ALIVE)
+    world[2][1].changeState(ALIVE)
+
+    world[10][11].changeState(ALIVE)
+    world[10][12].changeState(ALIVE)
+    world[10][13].changeState(ALIVE)
+
+    world[11][11].changeState(ALIVE)
+
+    world[12][11].changeState(ALIVE)
+    world[12][12].changeState(ALIVE)
+    world[12][13].changeState(ALIVE)
+
+    printWorld(world)
+
+def nextGeneration(world):
+    prevWorld = deepcopy(world)
+    for row in range(len(prevWorld)):
+        for column in range(len(prevWorld)):
+            neighbours = neighboursState(prevWorld, row, column)
+            if prevWorld[row][column].getState() == ALIVE:
+                if neighbours < 2 or neighbours > 3:
+                    world[row][column].changeState(DEAD)
+                elif neighbours == 2 or neighbours == 3:
+                    world[row][column].changeState(ALIVE)
+            elif prevWorld[row][column].getState() == DEAD and neighbours == 3:
+                world[row][column].changeState(ALIVE)
+    printWorld(world)
 
 # Get user input for size and generation:
 size = int(input('Grid sidelength (default 20): ') or SIZE)
-gen = int(input('Max generation: ')) + 1
+gen = int(input('Max generation: '))
 
 # Initialize Generation 0
 work = [[Cell() for x in range(size)] for y in range(size)]
 generateZero(work)
 
 # Compute:
-printWorld(work)
-'''
-for generation in range(0, gen+1):
-    printWorld(work)
+for generation in range(0, gen):
+    nextGeneration(work)
     time.sleep(1)
-'''
