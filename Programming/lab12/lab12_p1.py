dfca3201458a74736c978ce11981de1ee630526d
Iva Dobreva <iva95dobreva@gmail.com>
2016-06-02 17:48:26 +0900

    updated: task 1 and 2 should be done together

--- Programming/lab12/lab12_p1.py
+++ Programming/lab12/lab12_p1.py
@@ -1,11 +1,18 @@
 class Range:
     def __init__(self, start, upper):
-        self.__start = start
-        self.__end = upper
+        if start <= upper:
+            self.__start = start
+            self.__end = upper
+        else:
+            raise IndexError
 
     def __str__(self):
         output = str(self.__start) + '...' + str(self.__end)
         return output
 
-x = Range(12, 13)
-print(x)
+    def __lt__(self, range2):
+        for r2 in range(range2.__start, range2.__end+1):
+            for r1 in range(self.__start, self.__end+1 ):
+                if r1 >= r2:
+                    return False
+        return True
