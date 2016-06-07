7522e63b7b4efc2fa4e718c74d64d5e5e5d677e3
Iva Dobreva <iva95dobreva@gmail.com>
2016-06-07 13:36:40 +0900

    No change

--- Programming/lab12/lab12_p1.py
+++ Programming/lab12/lab12_p1.py
@@ -1,5 +1,6 @@
 class Range:
     def __init__(self, start, upper):
+        """Constructor function"""
         if start <= upper:
             self.__start = start
             self.__end = upper
@@ -7,10 +8,12 @@ class Range:
             raise IndexError
 
     def __str__(self):
+        """Representation of the outut """
         output = str(self.__start) + '...' + str(self.__end)
         return output
 
     def __lt__(self, range2):
+        """Implementation of the method larger than"""
         for r2 in range(range2.__start, range2.__end+1):
             for r1 in range(self.__start, self.__end+1 ):
                 if r1 >= r2:
