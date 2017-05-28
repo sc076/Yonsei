c7aaa68b107c5677af70d836c579e692c44a207f
Iva Dobreva <iva95dobreva@gmail.com>
2017-05-29 00:26:18 +0900

    Changes in the file naming and server file in order to read routes; connection to mongodb

--- CSI2109-01/!tardy/api/routes/!tardy.js
+++ CSI2109-01/!tardy/api/routes/!tardy.js
@@ -1,16 +1,16 @@
 'use strict';
 module.exports = function(app) {
-  var !tardy = require('../controllers/!tardyController');
+  var tardy = require('../controllers/tardyController');
 
 
-  // !tardy Routes
+  // tardy Routes
   app.route('/tasks')
-    .get(!tardy.list_all_tasks)
-    .post(!tardy.create_a_task);
+    .get(tardy.list_all_tasks)
+    .post(tardy.create_a_task);
 
 
   app.route('/tasks/:taskId')
-    .get(!tardy.read_a_task)
-    .put(!tardy.update_a_task)
-    .delete(!tardy.delete_a_task);
+    .get(tardy.read_a_task)
+    .put(tardy.update_a_task)
+    .delete(tardy.delete_a_task);
 };
--- CSI2109-01/!tardy/server.js
+++ CSI2109-01/!tardy/server.js
@@ -1,6 +1,18 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 8000;
+  port = process.env.PORT || 8000,  
+  mongoose = require('mongoose'),
+  Task = require('./api/models/!tardy'),
+  bodyParser = require('body-parser');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Tardydb');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var routes = require('./api/routes/!tardy');
+routes(app);
 
 app.listen(port);
 
